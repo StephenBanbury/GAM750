58fb9348309f6239eaecebe3bf9c9b167d55e842
Stephen Banbury <sbanbury@gmail.com>
2020-07-05 17:10:52 +0100

    Remove WebRTC. Working Mirror multiplayer network.

---
+++ Assets/HeadManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadManager : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Color newColor = new Color(Random.value, Random.value, Random.value, 1.0f);
+        gameObject.GetComponent<Renderer>().material.color = newColor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
